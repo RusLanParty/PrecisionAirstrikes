2f9882db47b73402a4541a655f9951f9cf7cecc7
Ruslan Libin <ruslanlib97@gmail.com>
2022-12-15 03:27:39 +0200

    1.4 (major update)
    -Fixed camera toggle responsiveness
    -Fixed planes exploding on spawn
    -Fixed planes not firing at the target (the only case they won't fire is when the flare stopped smoking, since the pilots can't see the target anymore they RTB)
    -Fixed planes exploding from their own shots
    -Fixed planes crashing into things
    -Pilots use brakes and other surfaces (make turns), flight feels more natural
    -Planes have turbulence now
    -Planes now spawn smarter, and always head in the correct direction
    -Fixed gear retracting/detracting mid flight
    -Planes now use the height of the target as an offset (if you throw a flare on the top of Chiliad, the planes will spawn at the height of the mountain plus the height you specified in the ini)
    -The strike covers larger area now, the coverage area moves forward with the plane
    -Added ability to tweak the radius of the strike via ini (if radius set to zero, the shots will draw a straight line)
    -Added ability to toggle blips via ini

---
+++ JetCam.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Airstrike;
+using GTA;
+
+namespace PrecisionAirstrike
+{
+    internal class JetCam : Script
+    {
+        public JetCam()
+        {
+            KeyUp += onKeyUp;
+        }
+        void onKeyUp(object sender, KeyEventArgs e)
+        {
+            if (Main.cameraSet && e.KeyCode == Main.camKey && !Main.cameraSet1)
+            {
+                Main.cameraSet1 = true;
+                World.RenderingCamera = Main.cam;
+            }
+            else if (Main.cameraSet && e.KeyCode == Main.camKey && Main.cameraSet1)
+            {
+                Main.cameraSet1 = false;
+                World.RenderingCamera = null;
+            }
+        }
+    }
+}
--- Main.cs
+++ Main.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using GTA;
 using GTA.Math;
 using GTA.Native;
+using NAudio.Gui;
 using NAudio.Wave;
 
 namespace Airstrike
@@ -17,14 +19,14 @@ namespace Airstrike
         List<Vehicle> jets = new List <Vehicle>();
         bool planeActive = false;
         bool isDone = false;
-        //Vehicle plane;
         Ped pilot;
         Ped owner;
         Blip blip;
-        Camera cam;
-        bool cameraSet;
-        bool cameraSet1;
-        bool dontPlay = false;
+        float spd=170f;
+        public static Camera cam;
+        public static bool cameraSet;
+        public static bool cameraSet1;
+        public static Keys camKey;
         int i;
         int j=0;
         int delayTime = 0;
@@ -38,15 +40,16 @@ namespace Airstrike
         public Main()
         {
             bool debug = false;
+            bool blipEnabled = Settings.GetValue<bool>("SETTINGS", "blips", true);
+            float radius = Settings.GetValue<float>("SETTINGS", "radius", 10f);
             bool resp = Settings.GetValue<bool>("SETTINGS", "responsibility", true);
             int timeOut = Settings.GetValue<int>("SETTINGS", "timeOut", 30000);
             bool jetAudio = Settings.GetValue<bool>("SETTINGS", "jetAudio", true);
             bool radioAudio = Settings.GetValue<bool>("SETTINGS", "radioAudio", true);
             string model = Settings.GetValue<String>("SETTINGS", "model", "lazer");
-            float height = Settings.GetValue<float>("SETTINGS", "height", 450.0f);
-            Keys camKey = Settings.GetValue<Keys>("SETTINGS", "camKey", Keys.NumPad5);
+            float height = Settings.GetValue<float>("SETTINGS", "height", 300.0f);
+            camKey = Settings.GetValue<Keys>("SETTINGS", "camKey", Keys.NumPad5);
             Tick += onTick;
-            KeyDown += onKeyUp;
             List<Vehicle> trash = new List<Vehicle>(World.GetAllVehicles(model));
             List<Ped> musor = new List<Ped>(World.GetAllPeds(PedHash.Blackops03SMY));
             disposeAudio();
@@ -62,23 +65,11 @@ namespace Airstrike
                 }
                 lazer.Delete();
             }
-            void onKeyUp(object sender, KeyEventArgs e)
-            {
-                if (cameraSet && e.KeyCode == camKey && !cameraSet1)
-                {
-                    cameraSet1 = true;
-                    World.RenderingCamera = cam;
-                }
-                else if (cameraSet && e.KeyCode == camKey && cameraSet1)
-                {
-                    cameraSet1 = false;
-                    World.RenderingCamera = null;
-                }
-            }
+           
             void onTick(object sender, EventArgs e)
             {
                // GTA.UI.Screen.ShowHelpText("output= " + output + " wave= " + wave, 1000, false, false); 
-                if (planeActive)
+                if (planeActive && jets.Count == 2)
                 {
                     timer++;
                     if (timer >= 2500)
@@ -87,24 +78,28 @@ namespace Airstrike
                         unCallPlane(true);
                         timer = 0;
                     }
-                    if (!jets[0].IsInRange(Game.Player.Character.Position, 3210f) || !jets[1].IsInRange(Game.Player.Character.Position, 3210f))
+                    if (jets.Count == 2)
                     {
-                        isDone = true;
+                        if (!jets[0].IsInRange(Game.Player.Character.Position, 3500f) || !jets[1].IsInRange(Game.Player.Character.Position, 3500f))
+                        {
+                            isDone = true;
+                            unCallPlane(false);
+                        }
+                    }
+                    if (isDone && jets.Count == 2)
+                    {
                         unCallPlane(false);
                     }
-                    if (isDone)
-                    {
-                        unCallPlane(false);
-                    }
-                    if (debug) { GTA.UI.Screen.ShowSubtitle("Distance to target: " + jets[0].Position.DistanceTo(Game.Player.Character.Position)); }
+
+                    if (debug && jets.Count == 2) { GTA.UI.Screen.ShowSubtitle("Distance to target: " + jets[0].Position.DistanceTo(Game.Player.Character.Position)); }
                     foreach (Vehicle jet in jets)
                     {
-                        jet.ForwardSpeed = 250;
+                        jet.ForwardSpeed = spd;
                     }
                 }
                 if (Function.Call<bool>(Hash.IS_EXPLOSION_IN_SPHERE, 22, Game.Player.Character.Position.X, Game.Player.Character.Position.Y, Game.Player.Character.Position.Z, 500f))
                 {
-                    if (debug) { GTA.UI.Screen.ShowHelpText("Timepass: " + timePass, 2000, false, false); }
+                    if (false) { GTA.UI.Screen.ShowHelpText("Timepass: " + timePass, 2000, false, false); }
                     newTime = Game.GameTime;
                     timePass = newTime - delayTime;
 
@@ -118,7 +113,7 @@ namespace Airstrike
                         {
                             callPlane(target);
                         }
-                        if (planeActive)
+                        if (planeActive && jets.Count == 2)
                         {
                             if (jets[0].HasCollided || jets[1].HasCollided)
                             {
@@ -133,62 +128,76 @@ namespace Airstrike
             {
                 Ped player = Game.Player.Character;
                 Vehicle plane;
-                Vector3 spwn = new Vector3(target.X, target.Y, height);
-                Vector3 spwn1 = new Vector3(target.X + 25f, target.Y + 10f, height + 2.0f);
+                Vector3 spwn = new Vector3(target.X, target.Y - 3000f, target.Z + height);
+                Vector3 spwn1 = new Vector3(target.X + 9.0f, target.Y - 3000f,target.Z + height);
                 for (int i = 1; i <= 2; i++)
                 {
                     if (debug) { GTA.UI.Screen.ShowHelpText("Jets are on the way...", 2000, false, false); }
-                    if (i == 1) { plane = World.CreateVehicle(model, spwn  + player.ForwardVector * -3000, player.Heading);
+                    if (i == 1) { plane = World.CreateVehicle(model, spwn, 0);
+                        float hed = Function.Call<float>(Hash.GET_HEADING_FROM_VECTOR_2D, target.X - plane.Position.X, target.Y - plane.Position.Y);
+                        plane.Heading = hed;
                         pilot = plane.CreatePedOnSeat(VehicleSeat.Driver, PedHash.Blackops03SMY);
                         plane.IsEngineRunning = true;
+                        plane.IsCollisionEnabled = false;
                         plane.LandingGearState = VehicleLandingGearState.Retracted;
-                        plane.ForwardSpeed = 300;
-                        blip = plane.AddBlip();
-                        blip.Color = BlipColor.BlueDark;
+                        plane.ForwardSpeed = spd;
+                        if (blipEnabled)
+                        {
+                            blip = plane.AddBlip();
+                            blip.Color = BlipColor.WhiteNotPure;
+                        }
                         pilot.RelationshipGroup = player.RelationshipGroup;
+                        plane.IsInvincible = true;
+                        Function.Call(Hash.TASK_PLANE_MISSION, pilot, plane, 0, 0, target.X, target.Y, target.Z, 4, 0f, 0f, hed,(target.Z + height) / 6.25f, (target.Z + height) / 6.25f);
                         jets.Add(plane);
+                       
                     }
                     else
-                    if (i == 2) { plane = World.CreateVehicle(model, spwn1  + player.ForwardVector * -3002, player.Heading);
+                    if (i == 2) { plane = World.CreateVehicle(model, spwn1, 0);
+                        float hed = Function.Call<float>(Hash.GET_HEADING_FROM_VECTOR_2D, target.X - plane.Position.X, target.Y - plane.Position.Y);
+                        plane.Heading = hed;
                         pilot = plane.CreatePedOnSeat(VehicleSeat.Driver, PedHash.Blackops03SMY);
                         plane.IsEngineRunning = true;
+                        plane.IsCollisionEnabled = false;
                         plane.LandingGearState = VehicleLandingGearState.Retracted;
-                        plane.ForwardSpeed = 300;
-                        blip = plane.AddBlip();
-                        blip.Color = BlipColor.BlueDark;
+                        plane.ForwardSpeed = spd;
+                        if (blipEnabled)
+                        {
+                            blip = plane.AddBlip();
+                            blip.Color = BlipColor.WhiteNotPure;
+                        }
                         pilot.RelationshipGroup = player.RelationshipGroup;
+                        plane.IsInvincible = true;
+                        Function.Call(Hash.TASK_PLANE_MISSION, pilot, plane, 0, 0, target.X, target.Y, target.Z, 4, 0f, 0f, hed, height / 6.25f, height / 6.25f);
                         jets.Add(plane);
+                        
                     }
                     isDone = false;
-                    //plane.IsInvincible = true;
-                    //Function.Call(Hash.TASK_PLANE_MISSION, pilot, plane, 0, 0, target.X + 2000f, target.Y, 300f, 0, 600f, 0f, target.ToHeading(), 300f, 300f);
-                    Wait(20);
+                    Wait(250);
                 }
-                if (radioAudio && !dontPlay)
+                if (radioAudio)
                 {
                     j = 0;
                     spamBlock = false;
-                    dontPlay = true;
                     int rndFx = Function.Call<int>(Hash.GET_RANDOM_INT_IN_RANGE, 2, 5);
                     playSfx(rndFx);
                 }
                 planeActive = true;
-                cam = World.CreateCamera(jets[0].Position, jets[0].Rotation, 80f);
-                Function.Call(Hash.ATTACH_CAM_TO_ENTITY, cam, jets[0], 0f, -15f, 2f, true);
+                cam = World.CreateCamera(jets[1].Position, jets[1].Rotation, 95f);
+                Function.Call(Hash.ATTACH_CAM_TO_ENTITY, cam, jets[1], 0f, -15f, 2f, true);
                 cameraSet = true;
             }
             void unCallPlane(bool instant)
             {
-                jets[0].ForwardSpeed = 250f;
-                jets[1].ForwardSpeed = 252f;
-                if (isDone)
+                if(jets.Count == 2)
                 {
-                    if (instant || !jets[0].IsInRange(Game.Player.Character.Position, 4010f) || !jets[1].IsInRange(Game.Player.Character.Position, 4010f))
+                    jets[0].ForwardSpeed = spd;
+                    jets[1].ForwardSpeed = spd;
+                }
+                if (isDone && jets.Count == 2)
+                {
+                    if (instant || !jets[0].IsInRange(Game.Player.Character.Position,3500f) || !jets[1].IsInRange(Game.Player.Character.Position, 3500f))
                     {
-                        if (!instant)
-                        {
-                            dontPlay = false;
-                        }
                         //if (instant) { GTA.UI.Screen.ShowSubtitle("Time out"); }
                         if (debug) { GTA.UI.Screen.ShowHelpText("Jets despawned...", 2000, false, false); }
                         planeActive = false;
@@ -216,57 +225,63 @@ namespace Airstrike
             void strike(Vector3 target, Ped pilot, List<Vehicle> jets)
             {
                 Ped player = Game.Player.Character;
-                    jets[0].ForwardSpeed = 250;
-                    jets[1].ForwardSpeed = 253;
-                if (jets[0].Position.DistanceTo(target) > 3210 || jets[1].Position.DistanceTo(target) > 3210)
+                if (jets.Count == 2)
+                {
+                    Function.Call(Hash.SET_PLANE_TURBULENCE_MULTIPLIER, jets[0], 1.0f);
+                    Function.Call(Hash.SET_PLANE_TURBULENCE_MULTIPLIER, jets[1], 1.0f);
+                    jets[0].ForwardSpeed = spd;
+                    jets[1].ForwardSpeed = spd;
+                    if (jets[0].Position.DistanceTo(target) > 3500f || jets[1].Position.DistanceTo(target) > 3500f)
                     {
                         isDone = true;
-                        unCallPlane(false);
+                        unCallPlane(true);
                     }
                     if (jets[0].Position.DistanceTo(target) < 600 || jets[1].Position.DistanceTo(target) < 600)
                     {
                         if (resp) { owner = player; }
                         else if (!resp) { owner = pilot; }
                         i = 1;
-                    if (jetAudio && j == 0)
-                    {
-                        j++;
-                        spamBlock = false;
-                        playSfx(1);
-                        // int Fx = Function.Call<int>(Hash.GET_RANDOM_INT_IN_RANGE, 0, 20);
-                        //if (Fx % 2 == 0) { playSfx(0); }
-                        // else if (Fx % 2 != 0) { playSfx(1); }
+                        if (jetAudio && j == 0)
+                        {
+                            j++;
+                            spamBlock = false;
+                            playSfx(1);
+                            // int Fx = Function.Call<int>(Hash.GET_RANDOM_INT_IN_RANGE, 0, 20);
+                            //if (Fx % 2 == 0) { playSfx(0); }
+                            // else if (Fx % 2 != 0) { playSfx(1); }
 
-                    }
-                    while (i <= 70 && !jets[0].IsDead && !isDone)
+                        }
+                        while (i <= 65 && !jets[0].IsDead && !isDone)
                         {
-                        jets[0].ForwardSpeed = 250f;
-                        jets[1].ForwardSpeed = 252f;
-                        World.ShootBullet(jets[0].Position, target.Around(Function.Call<float>(Hash.GET_RANDOM_FLOAT_IN_RANGE, 0f, 20f)), owner, WeaponHash.Railgun, 100, -1);
-                        World.ShootBullet(jets[1].Position, target.Around(Function.Call<float>(Hash.GET_RANDOM_FLOAT_IN_RANGE, 0f, 20f)), owner, WeaponHash.Railgun, 100, -1);
-                        if (debug) { GTA.UI.Screen.ShowHelpText("~r~SHOTS FIRED: " + i, 1000, false, false); }
-                            Wait(20);
+                            jets[0].ForwardSpeed = spd;
+                            jets[1].ForwardSpeed = spd;
+                            Vector3 offset = RotationToDirection(jets[0].Rotation);
+                            Vector3 jet0 = new Vector3(jets[0].Position.X, jets[0].Position.Y, jets[0].Position.Z -1.5f);
+                            Vector3 jet1 = new Vector3(jets[1].Position.X, jets[1].Position.Y, jets[1].Position.Z -1.5f);
+                            World.ShootBullet(jet0, target.Around(Function.Call<float>(Hash.GET_RANDOM_FLOAT_IN_RANGE, 0f, radius)) + offset * i , owner, WeaponHash.Railgun, 100, -1);
+                            World.ShootBullet(jet1, target.Around(Function.Call<float>(Hash.GET_RANDOM_FLOAT_IN_RANGE, 0f, radius)) + offset * i, owner, WeaponHash.Railgun, 100, -1);
+                            if (true) { GTA.UI.Screen.ShowHelpText("~r~SHOTS FIRED: " + i, 1000, false, false); }
+                            Wait(22);
                             i++;
-                        if (i >= 45)
-                        {
-                            delayTime = Game.GameTime;
-                            isDone = true;
-                            jets[0].ForwardSpeed = 250f;
-                            jets[1].ForwardSpeed = 253f;
-                            foreach (Vehicle jet in jets)
+                            if (i >= 65)
                             {
-                                Function.Call(Hash.TASK_PLANE_MISSION, jet.Driver, jet, 0, player, 0, 0, 0, 16, 1000f, 0f, 0f, 250f, 500f);
+                                delayTime = Game.GameTime;
+                                isDone = true;
+                                jets[0].ForwardSpeed = spd;
+                                jets[1].ForwardSpeed = spd;
+                                j = 0;
+                                
                             }
                         }
                     }
-                    }
-                    else if ((jets[0].Position.DistanceTo(target) > 600) && jets[0].Position.DistanceTo(target) < 2500)
+                    else if ((jets[0].Position.DistanceTo(target) > 600) && jets[0].Position.DistanceTo(target) < 3500)
                     {
                         timer++;
                         Wait(0);
-                        if (debug) { GTA.UI.Screen.ShowSubtitle("Emergency despawn timer: " + timer, 2000); }
+                        if (debug) { GTA.UI.Screen.ShowHelpText("Emergency despawn timer: " + timer, 2000, false, false); }
 
                     }
+                }
                 }
             }
             void playSfx(int fx)
@@ -335,6 +350,20 @@ namespace Airstrike
                        // GTA.UI.Screen.ShowHelpText("Disposed wave", 1000, true, false);
                 }   
             }
-            
+        public static Vector3 RotationToDirection(Vector3 Rotation)
+        {
+            float z = Rotation.Z;
+            float num = z * 0.0174532924f;
+            float x = Rotation.X;
+            float num2 = x * 0.0174532924f;
+            float num3 = Math.Abs((float)Math.Cos((double)num2));
+            return new Vector3
+            {
+                X = (float)((double)((float)(-(float)Math.Sin((double)num))) * (double)num3),
+                Y = (float)((double)((float)Math.Cos((double)num)) * (double)num3),
+                Z = (float)Math.Sin((double)num2)
+            };
         }
+
+    }
     }
